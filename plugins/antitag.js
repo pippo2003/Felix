335ecd166448cb17348218ba3e30d0d1f5af307e
pippo2003 <63353591+pippo2003@users.noreply.github.com>
2024-08-15 16:53:09 +0200

    Initial commit

---
+++ plugins/antitag.js
@@ -0,0 +1,15 @@
+let handler = m => m
+
+handler.before = async function (m, { participants, conn, isBotAdmin, isAdmin, args }) {
+let users = participants.map(u => conn.decodeJid(u.id))
+let frocio = users.lenght - 1
+let chat = global.db.data.chats[m.chat]
+let delet = m.key.participant
+let bang = m.key.id
+if (isBotAdmin && !isAdmin) {
+   if (m.mentionedJid.length >= 20) {
+conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+}}}
+
+export default handler
